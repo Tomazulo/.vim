64af6e6c91483db44e7cb8f44a89506055168d14
Tom Liao <liaojingguang@hotmail.com>
2013-12-20 15:12:50 -0500

    bundle install shellscript

---
+++ bin/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# script stolen directly from https://github.com/hashrocket/dotmatrix/blob/master/bin/vimbundles.sh
+#   modified to work with my .vim directory structure
+
+mkdir -p ~/.vim/bundle
+cd ~/.vim/bundle
+
+get_bundle() {
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    git pull --rebase
+  else
+    git clone "git://github.com/$1/$2.git"
+  fi
+  )
+}
+
+get_bundle scrooloose syntastic
+get_bundle vim-ruby vim-ruby
+
+vim -c 'call pathogen#helptags()|q'
+
